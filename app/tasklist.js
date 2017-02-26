c23f68dec82babe5838ff0cc99658e56711b3e70
zac <zknoebel@hawaii.edu>
2017-02-25 15:33:42 -1000

    Task 5: subscriptions

--- app/tasklist.js
+++ app/tasklist.js
@@ -4,6 +4,8 @@
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
+  Meteor.subscribe('tasks');
+
   Template.tasks.helpers({
     tasks: function() {
       return Tasks.find({}, {sort: {createdAt: -1}});
@@ -28,7 +30,9 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-
+  Meteor.publish('tasks', function() {
+    return Tasks.find({userId: this.userId});
+  })
 }
 
 Meteor.methods({
