75b1c7aa689f259becdfb1b6caaa89e74ba2dc40
zac <zknoebel@hawaii.edu>
2017-02-25 14:46:21 -1000

    Task 3: Tasklist form

--- app/tasklist.js
+++ app/tasklist.js
@@ -11,13 +11,26 @@ if (Meteor.isClient) {
   });
 
   Template.tasks.events({
-    "submit .add-task": function (event) {
-      var name = event.target.name.value;
+    "submit .add-task": function(event) {
+      let name = event.target.name.value;
+      Tasks.insert({
+        name: name, createdAt: new Date()
+      });
+      event.target.name.value = '';
+      return false;
+    },
+
+    "click .delete-task": function(event) {
+      if (confirm('Delete Task?')) {
+        Tasks.remove(this._id);
+      }
+      return false;
     }
-  });
+  })
 }
 
 if (Meteor.isServer) {
 
 }
+
 
