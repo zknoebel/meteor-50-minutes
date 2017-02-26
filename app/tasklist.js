b96046c8c53aebf116fe64493b4c0f1e075bcff8
zac <zknoebel@hawaii.edu>
2017-02-25 15:19:37 -1000

    Task 4: Tasklist authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -13,16 +13,14 @@ if (Meteor.isClient) {
   Template.tasks.events({
     "submit .add-task": function(event) {
       let name = event.target.name.value;
-      Tasks.insert({
-        name: name, createdAt: new Date()
-      });
+      Meteor.call('addTask', name);
       event.target.name.value = '';
       return false;
     },
 
     "click .delete-task": function(event) {
       if (confirm('Delete Task?')) {
-        Tasks.remove(this._id);
+        Meteor.call('deleteTask', this._id);
       }
       return false;
     }
@@ -32,5 +30,20 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  addTask: function(name){
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('No Access');
+    }
+    Tasks.insert({
+      name: name, createdAt: new Date(), userId: Meteor.userId()
+    });
+  },
+
+  deleteTask: function(taskId) {
+    Tasks.remove(taskId);
+  }
+})
 
 
